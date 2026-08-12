8d6f7b678cd254bba020e9e6cbedded9c0d4bcfd
ebubejoel <nwac0012@algonquinlive.com>
2026-08-11 22:51:39 -0400

    Add user route

---
+++ models/users.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    minLength: 10,
+    maxLength: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 1024,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+exports.userSchema = userSchema;
+exports.User = User;
